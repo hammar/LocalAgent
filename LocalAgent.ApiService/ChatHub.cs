5be0f74ba830f55753c4918093e1f6616a78275d
Karl Hammar <karl@karlhammar.com>
2025-05-18 12:18:46 -0700

    Function calling test.

--- LocalAgent.ApiService/ChatHub.cs
+++ LocalAgent.ApiService/ChatHub.cs
@@ -5,16 +5,30 @@ namespace LocalAgent.ApiService;
 
 public class ChatHub : Hub
 {
+    private ILogger<ChatHub> _logger;
     private readonly IChatClient _chatClient;
+    private readonly ChatOptions _chatOptions;
 
-    public ChatHub(IChatClient chatClient)
+    public ChatHub(ILogger<ChatHub> logger, IChatClient chatClient)
     {
+        _logger = logger;
         _chatClient = chatClient;
+
+        _chatOptions = new()
+        {
+            ToolMode = ChatToolMode.RequireSpecific("LogIWasRun"),
+            Tools = [AIFunctionFactory.Create(LogIWasRun, "LogIWasRun", "Logs that I was run")]
+        };
     }
 
     public async Task SendMessage(string message)
     {
-        var response = await _chatClient.GetResponseAsync(message);
+        var response = await _chatClient.GetResponseAsync(message, _chatOptions);
         await Clients.All.SendAsync("ReceiveMessage", response.Text);
+    }
+
+    private void LogIWasRun() 
+    {
+        _logger.LogInformation("I was run");
     }
 }
