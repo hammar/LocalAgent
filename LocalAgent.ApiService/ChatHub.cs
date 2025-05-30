cbce1cdef6bb134622d712107bfa60ce402ddc39
Karl Hammar <karl@karlhammar.com>
2025-05-29 23:23:14 -0700

    Rudimentary bot selection and system instruction support

--- LocalAgent.ApiService/ChatHub.cs
+++ LocalAgent.ApiService/ChatHub.cs
@@ -1,4 +1,6 @@
 using System.Reflection.Metadata.Ecma335;
+using LocalAgent.ApiService.Data;
+using LocalAgent.ApiService.Models;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.AI;
 
@@ -8,24 +10,48 @@ public class ChatHub : Hub
 {
     private ILogger<ChatHub> _logger;
     private readonly IChatClient _chatClient;
+    private readonly AppDbContext _dbContext;
     private readonly IToolProvider _toolProvider;
 
-    public ChatHub(ILogger<ChatHub> logger, IChatClient chatClient, IToolProvider toolProvider)
+    public ChatHub(ILogger<ChatHub> logger, IChatClient chatClient, AppDbContext dbContext, IToolProvider toolProvider)
     {
         _logger = logger;
         _chatClient = chatClient;
+        _dbContext = dbContext;
         _toolProvider = toolProvider;
     }
 
-    public async Task ProcessUserPrompt(List<ChatMessage> chatHistory)
+    public async Task ProcessUserPrompt(string AgentId, List<ChatMessage> chatHistory)
     {
+        // Return message to the originating client
         string originatingConnectionId = Context.ConnectionId;
+
+        // Get system prompt and prepend
+        var agent = _dbContext.Agents.Where(agent => agent.Id.ToString() == AgentId).First();
+        ChatMessage systemPromptMessage = new ChatMessage(ChatRole.System, agent.SystemInstructions);
+        chatHistory.Insert(0, systemPromptMessage);
+        
         var chatOptions = new ChatOptions
         {
             ToolMode = ChatToolMode.Auto,
             Tools = _toolProvider.GetTools().ToList(),
         };
         var responses = _chatClient.GetStreamingResponseAsync(chatHistory, chatOptions);
+        await foreach (ChatResponseUpdate response in responses)
+        {
+            await Clients.Client(originatingConnectionId).SendAsync("ProcessAgentResponse", response);
+        }
+    }
+
+    public async Task StartAgent(string AgentId)
+    {
+        string originatingConnectionId = Context.ConnectionId;
+        Guid requestedAgentId = Guid.Parse(AgentId);
+        var agent = _dbContext.Agents.Where(agent => agent.Id == requestedAgentId).First();
+        ChatMessage systemInstructionsMessage = new ChatMessage(ChatRole.System, agent.SystemInstructions);
+        ChatMessage startupMessage = new ChatMessage(ChatRole.User, "You are now starting to run. Please respond with your first greeting message.");
+        List<ChatMessage> chatHistory = [systemInstructionsMessage, startupMessage];
+        var responses = _chatClient.GetStreamingResponseAsync(chatHistory);
         await foreach (ChatResponseUpdate response in responses)
         {
             await Clients.Client(originatingConnectionId).SendAsync("ProcessAgentResponse", response);
